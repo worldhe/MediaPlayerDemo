78d05f838cdf8ae6e039869057d3731a3fb82cd4
Vincent Zhao <631523901@qq.com>
2019-12-15 09:53:58 +0800

    MediaPlayerDemo

--- app/src/main/java/com/rhct/mediaplayerdemo/MainActivity.java
+++ app/src/main/java/com/rhct/mediaplayerdemo/MainActivity.java
@@ -23,8 +23,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             findViewById(buttons[i]).setOnClickListener(this);
         }
 
-
-
     }
 
     @Override
---
+++ app/src/main/java/com/rhct/mediaplayerdemo/VideoViewHTTPActivity.java
@@ -0,0 +1,13 @@
+package com.rhct.mediaplayerdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VideoViewHTTPActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_view_http);
+    }
+}
